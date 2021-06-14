9b3af92592985125cfbf6e193e52e5b7df2cd89d
Gabriel <rev-x16.sassorii-wf@hotmail.com>
2021-06-13 20:06:17 -0500

    set env

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import app from './app'
-import path from 'path'
-// import { config as dotenv } from 'dotenv'
-// dotenv({ path: path.join(__dirname, '../', '.env') })
-app.start();
--- src/services/userService.ts
+++
@@ -1,6 +0,0 @@
-import User from '../models/user'
-abstract class UserService {
-    public abstract listUsers(): Promise<User[]>;
-    public abstract createUser(user:User):Promise<User>;
-}
-export default UserService
